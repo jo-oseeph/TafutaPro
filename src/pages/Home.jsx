99eb9c74885f36163d794a16e0e2173d25201ff5
jo-oseeph <realsejjo2001@gmail.com>
2025-06-23 22:53:48 +0300

    How it works and footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import PopularServices from '../components/PopularServices'  
+import HowItWorks from '../components/HowItWorks'
 const Home = () => {
   return (
       <>
    <Hero/>
     <PopularServices/>
+    <HowItWorks/>
    </>
     
   );
