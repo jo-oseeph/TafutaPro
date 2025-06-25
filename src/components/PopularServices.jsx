96d0525247bef8241ad1846e832d39d4b7cba16c
Onunga123 <onungachristopher363@gmail.com>
2025-06-25 10:32:55 -0700

    edited popularservices

--- src/components/PopularServices.jsx
+++ src/components/PopularServices.jsx
@@ -10,9 +10,9 @@ import {
 
 const services = [
   { name: "Plumbers", icon: Wrench, slug: "plumber" },
-  { name: "Capenters", icon: Hammer, slug: "cleaner" },
+  { name: "Carpenters", icon: Hammer, slug: "carpenter" },
   { name: "Electricians", icon: Zap, slug: "electrician" },
-  { name: "Moving", icon: Truck, slug: "moving" },
+  { name: "Delivery", icon: Truck, slug: "Delivery" },
   { name: "Tutors", icon: BookOpen, slug: "tutor" },
   { name: "Painters", icon: Paintbrush, slug: "painter" },
 ];
